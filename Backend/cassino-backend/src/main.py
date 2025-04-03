c94952bcef51aabe09d7af96ad516fd8f42163b4
RenatoSussumu <renatosussumutakahashijunior@gmail.com>
2025-04-02 22:21:27 -0300

    agora sim funciona :)

--- Backend/cassino-backend/src/main.py
+++ Backend/cassino-backend/src/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from src.api import user, bet
-from src.models.database import Base, engine
+from src.models.database import engine
 
-# Criar as tabelas no banco de dados
-Base.metadata.create_all(bind=engine)
+# Criação do lifespan para eventos de inicialização e finalização
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("🚀 Iniciando aplicação...")
+    yield  # O servidor roda enquanto não sair desse `yield`
+    print("🛑 Encerrando aplicação...")
+    await close_db_connection()
 
-app = FastAPI()
+# Função para fechar conexão com o banco
+async def close_db_connection():
+    print("🔌 Fechando conexão com o banco de dados...")
+    await engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
 
 # Adicionar rotas
 app.include_router(user.router, prefix="/user", tags=["User"])
